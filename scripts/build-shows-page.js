7de0f35a57720088232a0837b47529f3832b2704
laithajjan <laithajjan@gmail.com>
2023-01-21 22:27:34 -0700

    Added axios and implemented api calls plus styling for buttons and new features

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,56 +1,40 @@
-//Initializing an array of shows
-const shows = [
-    {
-        date: 'Mon Sept 06 2021',
-        venue: 'Ronald Lane',
-        location: 'San Francisco, CA'
-    },
-    {
-        date: 'Tue Sept 21 2021',
-        venue: 'Pier 3 East',
-        location: 'San Francisco, CA'
-    },
-    {
-        date: 'Fri Oct 15 2021',
-        venue: 'View Lounge',
-        location: 'San Francisco, CA'
-    },
-    {
-        date: 'Sat Nov 06 2021',
-        venue: 'Hyatt Agency',
-        location: 'San Francisco, CA'
-    },
-    {
-        date: 'Fri Nov 26 2021',
-        venue: 'Moscow Center',
-        location: 'San Francisco, CA'
-    },
-    {
-        date: 'Wed Dec 15 2021',
-        venue: 'Press Club',
-        location: 'San Francisco, CA'
-    }
-];
+const apiKey = '671ed9e7-8197-4a31-af1f-8d003b10d58e';
+const showsURL = `https://project-1-api.herokuapp.com/showdates/?api_key=${apiKey}`;
 
 
-//Finding the parent show to which other others should be appended to
+//declare function to format date
+function formatDate(date) {
+    correctDate = new Date(Number(date));
+    
+    const days = ['Sun','Mon','Tue','Wed','Thur','Fri','Sat'];
+    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec'];
+
+    const dy = days[correctDate.getDay()];
+    const mm = months[correctDate.getMonth()];
+    const dd = correctDate.getDate().toString().padStart(2, '0');
+    const yy = correctDate.getFullYear().toString();
+
+    return `${dy} ${mm} ${dd} ${yy}`;
+}
+
+
+//get the parent to which the shows would be appended
 const showSection = document.querySelector('.shows');
+let shows = [];
 
 
-//Creating Header section
+//shows section heading 
 const showsHeading = document.createElement('h2');
 showsHeading.classList.add('shows__heading');
 showsHeading.innerText = 'Shows';
 showSection.appendChild(showsHeading);
 
-
-// Creating Article section
+//shows section article 
 const showsArticle = document.createElement('article');
 showsArticle.classList.add('shows__body');
 showSection.appendChild(showsArticle);
 
-
-//Creating table
+//table head
 const tableHead = document.createElement('ul');
 tableHead.classList.add('shows__detail', 'shows__detail--top');
 
@@ -69,21 +53,35 @@ tableHeadLocation.classList.add('shows__detail-header');
 tableHeadLocation.innerText = 'Location';
 tableHead.appendChild(tableHeadLocation);
 
+const headerMargin = document.createElement('div');
+headerMargin.classList.add('shows__header-margin');
+tableHead.appendChild(headerMargin);
 
-//Adding table to Article Section
+//append table head to article section
 showsArticle.appendChild(tableHead);
 
 
-//Function for displaying shows
+// create a function to display all shows
 function displayShows() {
     shows.forEach((show) => {
-        //Body of the table
+
+        //table body
         const tableBody = document.createElement('ul');
         tableBody.classList.add('shows__container');
 
-        tableBody.classList.toggle('shows__container--selected');
+        function focusShow() {
+            const highlightedShows = document.querySelectorAll('.shows__container--selected');
+            if (highlightedShows.length > 0) {
+                highlightedShows.forEach((highlightedShow) => {
+                    highlightedShow.classList.replace('shows__container--selected', 'shows__container');
+                });
+            } 
+            tableBody.classList.replace('shows__container', 'shows__container--selected');
+        }
 
-        //List of Dates
+        tableBody.addEventListener('click', focusShow);
+
+        //table list for date
         const tableListDate = document.createElement('li');
         tableBody.appendChild(tableListDate);
 
@@ -94,10 +92,10 @@ function displayShows() {
 
         const showDateInfo = document.createElement('div');
         showDateInfo.classList.add('shows__date');
-        showDateInfo.innerText = show.date;
+        showDateInfo.innerText = formatDate(show.date);
         tableListDate.appendChild(showDateInfo);
 
-        //List of Venues
+        //table list for venue
         const tableListVenue = document.createElement('li');
         tableBody.appendChild(tableListVenue);
 
@@ -108,10 +106,10 @@ function displayShows() {
 
         const showVenueInfo = document.createElement('div');
         showVenueInfo.classList.add('shows__venue');
-        showVenueInfo.innerText = show.venue;
+        showVenueInfo.innerText = show.place;
         tableListVenue.appendChild(showVenueInfo);
 
-        //List of Locations
+        //table list for location
         const tableListLocation = document.createElement('li');
         tableBody.appendChild(tableListLocation);
 
@@ -125,23 +123,30 @@ function displayShows() {
         showLocationInfo.innerText = show.location;
         tableListLocation.appendChild(showLocationInfo);
 
-        //List of buying
+        //table list for button
         const tableListButton = document.createElement('li');
         tableBody.appendChild(tableListButton);
 
         const button = document.createElement('a');
-        button.setAttribute('href', '');
+        button.setAttribute('href', 'https://www.bandsintown.com/');
         button.setAttribute('target', '_blank');
         button.classList.add('shows__button');
         button.innerText = 'Buy Tickets';
         tableListButton.appendChild(button);
 
-        //Adding comment to Articles
+        //append comment to article section
         showsArticle.appendChild(tableBody);
-        console.log(tableBody);
     });
 }
 
 
-//Function to Display Shows
-displayShows();
+//make axios request for shows
+axios 
+    .get(showsURL)
+    .then((response) => {
+        shows = response.data;
+        displayShows();
+    })
+    .catch((error) => {
+        console.log(error);
+    });
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,96 +1,178 @@
-//create an array of comments
-const comments = [
-    {
-        name: 'Connor Walton',
-        timeStamp: '02/17/2021',
-        image: '',
-        commentText: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.'
-    },
-    {
-        name: 'Emilie Beach',
-        timeStamp: '01/09/2021',
-        image: '',
-        commentText: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.'
-    },
-    {
-        name: 'Miles Acosta',
-        timeStamp: '12/20/2020',
-        image: '',
-        commentText: 'I can\'t stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can\'t get enough.'
-    }
+const apiKey = '671ed9e7-8197-4a31-af1f-8d003b10d58e';
+const getURL = `https://project-1-api.herokuapp.com/comments/?api_key=${apiKey}`;
+const postURL = `https://project-1-api.herokuapp.com/comments/?api_key=${apiKey}`;
+
+
+//declare function to format name to desired case
+function formatName(string) {
+    const names = string.split(' ');
+    const newNames = names.map((name) => {
+        return ((name.charAt(0).toUpperCase()) + (name.slice(1)).toLowerCase());
+    });
+    const formattedName = newNames.join(' ');
+    return formattedName;
+}
+
+
+//declare function to format date to dynamic date
+const intervals = [
+    { label: 'year', seconds: 31536000 },
+    { label: 'month', seconds: 2592000 },
+    { label: 'day', seconds: 86400 },
+    { label: 'hour', seconds: 3600 },
+    { label: 'minute', seconds: 60 },
+    { label: 'second', seconds: 1 },
+    { label: 'second', seconds: -10 }
 ];
+
+function formatDate(date) {
+    const seconds = Math.floor((Date.now() - date) / 1000);
+    const interval = intervals.find((interval) => {
+        return interval.seconds < seconds;
+    });
+    const count = Math.floor(seconds / interval.seconds);
+    return `${count} ${interval.label}${count !== 1 ? 's' : ''} ago`;
+}
 
 
 //get the parent to which the comments would be appended
 const commentSection = document.querySelector('.comment__section');
+let comments = [];
 
 
-// create a function to display each comment
-function displayComment(item) {
-    const userComment = document.createElement('div');
-    userComment.classList.add('comment__old');
+// create a function to display all comments
+function displayComments() {
 
-    //image element
-    if (item.image === '') {
-        const imageDiv = document.createElement('div');
-        imageDiv.classList.add('comment__avatar-missing');
-        userComment.appendChild(imageDiv);
-    } else {
-        const userAvatar = document.createElement('img');
-        userAvatar.setAttribute('src', item.image);
-        userAvatar.classList.add('comment__avatar', 'comment__avatar--new');
-        userComment.appendChild(userAvatar);
-    }
+    commentSection.innerHTML = '';
 
-    //comment container
-    const commentContainer = document.createElement('div');
-    commentContainer.classList.add('comment__old-container');
-    userComment.appendChild(commentContainer);
+    comments.forEach((comment) => {
+        const userComment = document.createElement('div');
+        userComment.classList.add('comment__old');
 
-    //user info
-    const userInfo = document.createElement('div');
-    userInfo.classList.add('comment__info');
-    commentContainer.appendChild(userInfo);
+        //image element
+        if (!comment.image) {
+            const imageDiv = document.createElement('div');
+            imageDiv.classList.add('comment__avatar-missing');
+            userComment.appendChild(imageDiv);
+        } else {
+            const userAvatar = document.createElement('img');
+            userAvatar.setAttribute('src', comment.image);
+            userAvatar.classList.add('comment__avatar', 'comment__avatar--new');
+            userComment.appendChild(userAvatar);
+        }
 
-    //user name
-    const userName = document.createElement('div');
-    userName.classList.add('comment__writer');
-    userName.innerText = item.name;
-    userInfo.appendChild(userName);
+        //comment container
+        const commentContainer = document.createElement('div');
+        commentContainer.classList.add('comment__old-container');
+        userComment.appendChild(commentContainer);
 
-    //date
-    const date = document.createElement('div');
-    date.classList.add('comment__date');
-    date.innerText = item.timeStamp;
-    userInfo.appendChild(date);
+        //user info
+        const userInfo = document.createElement('div');
+        userInfo.classList.add('comment__info');
+        commentContainer.appendChild(userInfo);
 
-    //comment text
-    const text = document.createElement('p');
-    text.innerText = item.commentText;
-    commentContainer.appendChild(text);
+        //user name
+        const userName = document.createElement('div');
+        userName.classList.add('comment__writer');
+        userName.innerText = comment.name;
+        userInfo.appendChild(userName);
 
-    //append comment to webpage
-    commentSection.appendChild(userComment);
+        //date
+        const date = document.createElement('div');
+        date.classList.add('comment__date');
+        date.innerText = formatDate(comment.timestamp);
+        userInfo.appendChild(date);
+
+        //comment text
+        const text = document.createElement('p');
+        text.innerText = comment.comment;
+        commentContainer.appendChild(text);
+
+        //comment buttons
+        const commentButtons = document.createElement('div');
+        commentButtons.classList.add('comment__buttons');
+        commentContainer.appendChild(commentButtons); 
+
+        //like button
+        const likeButton = document.createElement('button');
+        likeButton.classList.add('comment__like');
+        if (comment.likes === 1) {
+            likeButton.innerText = `${comment.likes} Like`;
+        } else {
+            likeButton.innerText = `${comment.likes} Likes`;
+        }
+        commentButtons.appendChild(likeButton);
+
+        const likeComment = ((event) => {
+            event.preventDefault(); 
+
+                comment.likes++;
+                if (comment.likes === 1) {
+                    event.target.innerText = `${comment.likes} Like`;
+                } else {
+                    event.target.innerText = `${comment.likes} Likes`;
+                }
+                
+                axios
+                    .put(`https://project-1-api.herokuapp.com/comments/${comment.id}/like/?api_key=${apiKey}`)
+                    .then(() => {
+                    })
+                    .catch(() => {
+                        const likeError = document.createElement('span');
+                        likeError.classList.add('comment__validation');
+                        likeError.innerText = 'I\'m sorry you cannot like the comment right now, please try again later.'
+                        commentButtons.appendChild(likeError);
+                    });
+        });
+        
+        likeButton.addEventListener('click', likeComment);
+
+        //delete button
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('comment__delete');
+        deleteButton.innerText = 'Delete';
+        commentButtons.appendChild(deleteButton);
+
+        const deleteComment = ((event) => {
+            event.preventDefault(); 
+
+            commentSection.removeChild(userComment);
+            axios
+                .delete(`https://project-1-api.herokuapp.com/comments/${comment.id}/?api_key=${apiKey}`)
+                .then(() => {
+                })
+                .catch(() => {
+                    const deleteError = document.createElement('span');
+                    deleteError.classList.add('comment__validation');
+                    deleteError.innerText = 'I\'m sorry you cannot delete the comment right now, please try again later.'
+                    commentButtons.appendChild( deleteError);
+                });
+        });
+
+        deleteButton.addEventListener('click', deleteComment);
+
+        //append comment to webpage
+        commentSection.appendChild(userComment);
+    });  
 }
 
 
-// call the function to display each comment on the bio page
-comments.forEach((comment) => {
-    displayComment(comment);
-});
+//make axios request for comments
+axios
+    .get(getURL)
+    .then((response) => {
+        comments = response.data.reverse();
+        displayComments(); 
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
 
-//declare function to get date
-function formatDate(date) {
-    const mm = (date.getMonth() + 1).toString().padStart(2, '0');
-    const dd = date.getDate().toString().padStart(2, '0');
-    const yyyy = date.getFullYear().toString();
-    return `${mm}/${dd}/${yyyy}`;
-}
-
-//create function to add new comment to the page
+//create function to post new comment
 const addComment = ((event) => {
-    event.preventDefault();
+
+    event.preventDefault(); 
 
     commentSection.innerHTML = '';
 
@@ -98,33 +180,44 @@ const addComment = ((event) => {
     const formFieldName = document.querySelector('.comment__input-name');
     const formFieldComment = document.querySelector('.comment__input-conversation');
     const formFieldvalidation = document.querySelector('.comment__validation');
-    const userName = event.target.name.value;
-    const userDate = formatDate(new Date());
-    const userImage = '';
+
+    const userName = formatName(event.target.name.value);
     const userComment = event.target.comment.value;
 
-    if (userName === '' || userComment === '') {
+    if(userName === '' || userComment === '') {
         formFieldName.classList.add('comment__input-error');
         formFieldComment.classList.add('comment__input-error');
         formFieldvalidation.innerText = '***Please check that you have entered your name or comment***';
+        displayComments();
     } else {
         formFieldName.classList.remove('comment__input-error');
         formFieldComment.classList.remove('comment__input-error');
         formFieldvalidation.innerText = '';
 
         newComment.name = userName;
-        newComment.timeStamp = userDate;
-        newComment.image = userImage;
-        newComment.commentText = userComment;
-
-        comments.unshift(newComment);
+        newComment.comment = userComment;
+    
+        axios
+            .post(postURL, newComment)
+            .then(() => {
+                return axios
+                    .get(getURL)
+                    .then((response) => {
+                        comments = response.data.reverse();
+                        displayComments(); 
+                    })
+                    .catch(() => {
+                    });
+            })
+            .catch(() => {
+                const postError = document.createElement('span');
+                postError.classList.add('comment__validation');
+                postError.innerText = 'I\'m sorry you cannot post a comment right now, please try again later.'
+                formFieldvalidation.appendChild( postError);
+            });
 
         event.target.reset();
     }
-
-    comments.forEach((comment) => {
-        displayComment(comment);
-    });
 });
 
 
